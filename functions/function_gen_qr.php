1c1140cfdbe4d136e1fb8b6d82de84124608c2dd
kasidit wansudon <kasidit.wans@gmail.com>
2025-05-18 19:34:32 +0700

    update part qr

--- functions/function_gen_qr.php
+++ functions/function_gen_qr.php
@@ -2,7 +2,7 @@
 // function_gen_qr.php
 function generateQRCode($text) {
     include_once "./phpqrcode-master/qrlib.php";
-    $filename = "../cache/_qr" . time() . ".png";
+    $filename = "./cache/_qr" . time() . ".png";
     QRcode::png($text, $filename, QR_ECLEVEL_L, 4);
     return $filename;
 }
